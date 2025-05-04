d174c6c47140d0e7e5094dde374de0eec59a1a08
Michael Felipe <michaelfelipe180@gmail.com>
2025-05-04 18:56:52 -0300

    feat:67fb1e77deda6279579a5e65

---
+++ frontend/src/login_firebase.js
@@ -0,0 +1,26 @@
+// login_firebase.js
+import { auth } from './firebase-config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+export async function login(email, senha) {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, senha);
+        const token = await userCredential.user.getIdToken();
+
+        const response = await fetch("http://localhost:8080/auth", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) throw new Error("Erro ao buscar usuário autenticado");
+
+        const usuario = await response.json();
+        console.log("Usuário autenticado:", usuario);
+        return usuario;
+    } catch (error) {
+        console.error("Erro no login:", error.message);
+        throw error;
+    }
+}
